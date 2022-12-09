711ae30bfa389e72f5596f22f5f93295b609ca59
romamale.com@gmail.com <romamale.com@gmail.com>
2022-12-09 11:14:31 +0200

    prog

--- zalik.py
+++ zalik.py
@@ -1,12 +1,12 @@
 file=open("h.txt")
 x=file.read()
 podil=x.split('\n')
-
+print('Знайдені виміри:')
 for line in podil:
     k=line.split(" ")
     print(k[1])
 
-search=input("Введіть число формату AAA")
+search=input("Введіть число формату AAA ")
 
 for line in podil:
     k = line.split(" ")
